d4ba4292550d7a4d31e4d300686f0a179f4451fe
marialegl <psico.marialegl@gmail.com>
2024-04-08 19:16:26 -0500

    Tasks 3  Prepare a MySQL server for the project. A database, a new user with all privileges on the database and with the SELECT privilege on the performance_schema database

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,7 @@
+-- Prepare a MySQL server for the project
+-- A database, a new user with all privileges on the database
+-- and with the SELECT privilege on the performance_schema database
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
