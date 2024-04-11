2df0999ebfca7cdefc48b2b5a1e86268c9146060
marialegl <psico.marialegl@gmail.com>
2024-04-11 18:53:17 -0500

    Tasks 8 update class attribute places in user.py

--- models/user.py
+++ models/user.py
@@ -2,23 +2,24 @@
 """This module defines a class User"""
 from models.base_model import BaseModel
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+
 
 class User(BaseModel):
     """This class defines a user by various attributes"""
-    email = ''
-    password = ''
-    first_name = ''
-    last_name = ''
-
-"""class User(BaseModel):
-    This class defines a user by various attributes
-    __tablename__ = 'users'
+    __tablename__ = "users"
     
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
     first_name = Column(String(128))
     last_name = Column(String(128))
+    
+    places = relationship(
+        "Place",
+        backref='user',
+        cascade="all, delete-orphan",
+        passive_deletes=True)
 
     def __init__(self, *args, **kwargs):
-        Initializes User instance
-        super().__init__(*args, **kwargs)"""
+        """Initializes User instance"""
+        super().__init__(*args, **kwargs)
