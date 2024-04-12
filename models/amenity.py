3c2bba6eb7964803e5e800c5c272fd91ff2d7fcc
marialegl <psico.marialegl@gmail.com>
2024-04-12 15:24:19 -0500

    Spelling error correction in amenity.py

--- models/amenity.py
+++ models/amenity.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
 class Amenity(BaseModel, Base):
-    name = Column(String(128), nulleable=False)
+    name = Column(String(128), nullable=False)
     __tablename__ = "amenities"
     place_amenities = relationship(
         "Place",
