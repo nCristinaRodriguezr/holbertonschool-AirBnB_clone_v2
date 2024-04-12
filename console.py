4524a41a08b3fd9c068bdc15e9593390abe7b46a
nCristinaRodriguezr <ncristina.rodriguezr@gmail.com>
2024-04-11 19:48:46 -0500

    point 6 new fixing

--- console.py
+++ console.py
@@ -4,13 +4,13 @@ import cmd
 import sys
 import re
 from models.base_model import BaseModel
-from models.__init__ import storage
 from models.user import User
 from models.place import Place
 from models.state import State
 from models.city import City
 from models.amenity import Amenity
 from models.review import Review
+from models.__init__ import storage
 
 
 class HBNBCommand(cmd.Cmd):
