d1f4ef289cbcc0372f867f967144974fed153dc7
nataly <ncristina.rodriguezr@gmail.com>
2024-04-25 15:29:00 -0500

    point 0

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+A script that starts a Flask web application
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    """
+    Return a
+    """
+    return "Hello HBNB!"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
