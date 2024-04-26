bd62e57bfacac63466c4f1505749c0ccb9773285
nataly <ncristina.rodriguezr@gmail.com>
2024-04-26 11:01:29 -0500

    point 5

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -47,7 +47,7 @@ def display_number(n):
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def display_number_template(n):
     if isinstance(n, int):
-        return render_template('number.html', number=n)
+        return render_template('5-number.html', number=n)
     else:
         return 'Error: Not an integer'
 
