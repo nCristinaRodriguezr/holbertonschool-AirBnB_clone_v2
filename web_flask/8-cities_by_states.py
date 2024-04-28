1d0c54c5b3565eb86e91cfd268ebf5f9ac1e6ec9
nataly <ncristina.rodriguezr@gmail.com>
2024-04-28 16:34:24 -0500

    point 9

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+A script that starts a Flask web application
+"""
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+from models.city import City
+
+app = Flask(__name__)
+
+
+@app.teardown_appcontext
+def close_session(exception):
+    """Closes the current SQLAlchemy Session"""
+    storage.close()
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def states_list():
+    states = storage.all(State).values()
+    sorted_states = sorted(states, key=lambda state: state.name)
+    state_cities = {}
+    for state in sorted_states:
+        if storage.__class__.__name__ == "DBStorage":
+            cities = [city for city in state.cities]
+        else:
+            cities = storage.all(City).values()
+            cities = [city for city in cities if city.state_id == state.id]
+        state_cities[state.id] = cities
+    return render_template(
+            '8-cities_by_states.html',
+            sorted_states=sorted_states,
+            state_cities=state_cities
+        )
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
