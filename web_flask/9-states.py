9a9e697c19c654815e4892be3c8c819a10683f71
nataly <ncristina.rodriguezr@gmail.com>
2024-04-28 22:25:13 -0500

    point 10

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -2,6 +2,7 @@
 """
 A script that starts a Flask web application
 """
+from sqlalchemy.orm.exc import NoResultFound
 from flask import Flask, render_template
 from models import storage
 from models.state import State
@@ -27,11 +28,16 @@ def show_states():
 def show_state(state_id):
     if storage.__class__.__name__ == "DBStorage":
         session = storage._DBStorage__session
-        states_list = session.query(State).filter(State.id == state_id)
-        state = states_list[0]
+        try:
+            state = session.query(State).filter(State.id == state_id).one()
+        except NoResultFound:
+            state = None
     else:
         state = storage.get(State, state_id)
-    cities = sorted(state.cities, key=lambda city: city.name)
+    if state:
+        cities = sorted(state.cities, key=lambda city: city.name)
+    else:
+        cities = []
     return render_template('9-states.html', state=state, cities=cities)
 
 
